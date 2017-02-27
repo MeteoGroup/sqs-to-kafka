4a29c273d22493319b0c60037cbe03f6fcfd3380
Eric Karge <eric.karge@meteogroup.com>
2017-02-27 14:01:30 +0100

    Fix password leakage in deploy script

--- travis/deploy-to-dockerhub.sh
+++ travis/deploy-to-dockerhub.sh
@@ -1,13 +1,14 @@
-#!/bin/sh -x
+#!/bin/sh
 
 if [ "$TRAVIS" != true ]; then
   echo "This script is intended to run within the travis build only" 1>&2
   exit 1
 fi
 
-exec 3>&2 2>&- # disable logging to ensure we don't leak our docker secrets
-docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS" 2>&3
-exec 2>&3  3>&- # re-enable logging
+( # temporarily disable shell debugging, docker hub password would leak otherwise
+  set +x
+  docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+)
 
 docker tag "meteogroup/sqs-to-kafka:$COMMIT" "meteogroup/sqs-to-kafka:$IMAGE_TAG"
 docker tag "meteogroup/sqs-to-kafka:$COMMIT" "meteogroup/sqs-to-kafka:latest"
