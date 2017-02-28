647916aa6b78bfe1c99bb5984eb8484cee04ac10
Eric Karge <eric.karge@meteogroup.com>
2017-02-28 11:34:17 +0100

    Enable multi-platform build

---
+++ travis/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+if [ "$TRAVIS" != true ]; then
+  echo "This script is intended to run within the travis build only" 1>&2
+  exit 1
+fi
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o sqs-to-kafka-linux64-static
+ln sqs-to-kafka-linux64-static sqs-to-kafka
+GOOS=linux GOARCH=amd64 go build -o sqs-to-kafka-linux64
+GOOS=darwin GOARCH=amd64 go build -o sqs-to-kafka-darwin64
+
+docker build -t "meteogroup/sqs-to-kafka:$COMMIT" .
+test/test.sh "$COMMIT"
