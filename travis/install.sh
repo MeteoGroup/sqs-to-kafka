8da708b4dc2bd8a596058b52a8bef0a7be0180aa
Eric Karge <eric.karge@meteogroup.com>
2017-02-28 13:14:31 +0100

    Minor tweaks for release builds
    ...including building windows binaries.

--- travis/install.sh
+++ travis/install.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 if [ "$TRAVIS" != true ]; then
   echo "This script is intended to run within the travis build only" 1>&2
   exit 1
 fi
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o sqs-to-kafka-linux64-static
-ln sqs-to-kafka-linux64-static sqs-to-kafka
-GOOS=linux GOARCH=amd64 go build -o sqs-to-kafka-linux64
-GOOS=darwin GOARCH=amd64 go build -o sqs-to-kafka-darwin64
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o sqs-to-kafka-linux64
+ln sqs-to-kafka-linux64 sqs-to-kafka
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o sqs-to-kafka-darwin64
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o sqs-to-kafka-windows64.exe
 
 docker build -t "meteogroup/sqs-to-kafka:$COMMIT" .
 test/test.sh "$COMMIT"
